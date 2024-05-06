1cc1dd3f944d8bd2c219ec3e79bc212b11c22c05
buithy <buithy31@gmail.com>
2024-05-06 19:20:14 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_project',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pandas==1.3.3',
+        'numpy==1.21.2',
+        'matplotlib==3.4.3',
+    ],
+)
