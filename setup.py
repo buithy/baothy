4eb61764c421ad6dc6055122c41348bb8df1e4ee
buithy <buithy31@gmail.com>
2024-05-06 19:46:07 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='finalproject_baothy',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pandas==1.3.3',
+        'numpy==1.21.2',
+        'matplotlib==3.4.3',
+    ],
+)
+
 
